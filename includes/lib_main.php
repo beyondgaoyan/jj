3f76a3b4c1dc6612ab3cea5a221bd8c0db328090
gaoyan <405704570@qq.com>
2014-12-01 17:32:18 +0800

    no message

--- includes/lib_main.php
+++ includes/lib_main.php
@@ -2068,4 +2068,70 @@ function url_domain()
     return $root;
 }
 
+/**
+ * 查询咨询内容
+ *
+ * @access  public
+ * @params  integer     $id
+ * @params  integer     $type
+ * @params  integer     $page
+ * @return  array
+ */
+function assign_zxcomment($id, $type, $page = 1)
+{
+    /* 取得评论列表 */
+    $count = $GLOBALS['db']->getOne('SELECT COUNT(*) FROM ' .$GLOBALS['ecs']->table('zxcomment').
+           " WHERE id_value = '$id' AND comment_type = '$type' AND status = 1 AND parent_id = 0");
+    $size  = !empty($GLOBALS['_CFG']['comments_number']) ? $GLOBALS['_CFG']['comments_number'] : 5;
+
+    $page_count = ($count > 0) ? intval(ceil($count / $size)) : 1;
+
+    $sql = 'SELECT * FROM ' . $GLOBALS['ecs']->table('zxcomment') .
+            " WHERE id_value = '$id' AND comment_type = '$type' AND status = 1 AND parent_id = 0".
+            ' ORDER BY comment_id DESC';
+    $res = $GLOBALS['db']->selectLimit($sql, $size, ($page-1) * $size);
+
+    $arr = array();
+    $ids = '';
+    while ($row = $GLOBALS['db']->fetchRow($res))
+    {
+        $ids .= $ids ? ",$row[comment_id]" : $row['comment_id'];
+        $arr[$row['comment_id']]['id']       = $row['comment_id'];
+        $arr[$row['comment_id']]['email']    = $row['email'];
+        $arr[$row['comment_id']]['username'] = $row['user_name'];
+        $arr[$row['comment_id']]['content']  = str_replace('\r\n', '<br />', htmlspecialchars($row['content']));
+        $arr[$row['comment_id']]['content']  = nl2br(str_replace('\n', '<br />', $arr[$row['comment_id']]['content']));
+        $arr[$row['comment_id']]['rank']     = $row['comment_rank'];
+        $arr[$row['comment_id']]['add_time'] = local_date($GLOBALS['_CFG']['time_format'], $row['add_time']);
+    }
+    /* 取得已有回复的评论 */
+    if ($ids)
+    {
+        $sql = 'SELECT * FROM ' . $GLOBALS['ecs']->table('zxcomment') .
+                " WHERE parent_id IN( $ids )";
+        $res = $GLOBALS['db']->query($sql);
+        while ($row = $GLOBALS['db']->fetch_array($res))
+        {
+            $arr[$row['parent_id']]['re_content']  = nl2br(str_replace('\n', '<br />', htmlspecialchars($row['content'])));
+            $arr[$row['parent_id']]['re_add_time'] = local_date($GLOBALS['_CFG']['time_format'], $row['add_time']);
+            $arr[$row['parent_id']]['re_email']    = $row['email'];
+            $arr[$row['parent_id']]['re_username'] = $row['user_name'];
+        }
+    }
+    /* 分页样式 */
+    //$pager['styleid'] = isset($GLOBALS['_CFG']['page_style'])? intval($GLOBALS['_CFG']['page_style']) : 0;
+    $pager['page']         = $page;
+    $pager['size']         = $size;
+    $pager['record_count'] = $count;
+    $pager['page_count']   = $page_count;
+    $pager['page_first']   = "javascript:gotozxPage(1,$id,$type)";
+    $pager['page_prev']    = $page > 1 ? "javascript:gotozxPage(" .($page-1). ",$id,$type)" : 'javascript:;';
+    $pager['page_next']    = $page < $page_count ? 'javascript:gotozxPage(' .($page + 1) . ",$id,$type)" : 'javascript:;';
+    $pager['page_last']    = $page < $page_count ? 'javascript:gotozxPage(' .$page_count. ",$id,$type)"  : 'javascript:;';
+
+    $cmt = array('comments' => $arr, 'pager' => $pager);
+
+    return $cmt;
+}
+
 ?>
